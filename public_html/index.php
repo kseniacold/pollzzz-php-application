03e7bc2055276f1847c6fddcd374e192eea6d11b
Ksenia Koldaeva <kseniacold@gmail.com>
2017-07-17 12:00:18 -0700

    Basic structure. index.php with head.php included.

---
+++ public_html/index.php
@@ -0,0 +1,6 @@
+<?php    
+    // load up your config file
+    require_once("/path/to/resources/config.php");
+     
+    require_once(TEMPLATES_PATH . "/header.php");
+?>
---
+++ resources/config.php
@@ -0,0 +1,26 @@
+<?php
+ 
+/**
+ *  config.php will be included to every page of the website
+ *  contains configuration information about the application
+ */
+ 
+$config = array(
+    "paths" => array(
+        "resources" => "/resources"
+    )
+);
+ 
+
+/**
+    Creating constants for heavily used paths makes things a lot easier.
+    ex. require_once(TEMPLATE_PATH . "header.php")
+*/
+
+defined("LIBRARY_PATH")
+    or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library')); // concatinate the directory of current included file (__FILE__)
+     
+defined("TEMPLATES_PATH")
+    or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
+ 
+?>
